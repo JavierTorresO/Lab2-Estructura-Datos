2289d633ff909c29a5457ad68f4ac59b6db2e9ee
JavierTorresO <javitorres2022@udec.cl>
2025-05-30 20:39:21 -0400

    Creacion algoritmo insertion sort. Se crea un main muy basico para probar que funciona

--- algoritmos/main.cpp
+++ algoritmos/main.cpp
@@ -1,3 +1,19 @@
 #include <iostream>
+#include <vector>
+#include "insertion/insertion_sort.h" 
 
+int main() {
+    std::vector<int32_t> datos = {5, 2, 9, 1, 5, 6};
+    insertionSort(datos);
+
+    std::cout << "Datos ordenados: ";
+    for (int32_t num : datos) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
+
+//correr con: g++ main.cpp insertion/insertion_sort.cpp -o programa
 
